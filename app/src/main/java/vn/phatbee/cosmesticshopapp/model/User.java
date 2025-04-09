f4582b44900332417bef06f03ede3fe2829b5aa5
PhatBee <vinhphatst1235@gmail.com>
2025-04-09 14:03:52 +0700

    F2. LogIn

---
+++ app/src/main/java/vn/phatbee/cosmesticshopapp/model/User.java
@@ -0,0 +1,62 @@
+package vn.phatbee.cosmesticshopapp.model;
+
+public class User {
+    private Long userId;
+    private String username;
+    private String password;
+    private String email;
+    private String gender;
+    private String image;
+
+    public User() {
+
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/vn/phatbee/cosmesticshopapp/retrofit/ApiService.java
+++ app/src/main/java/vn/phatbee/cosmesticshopapp/retrofit/ApiService.java
@@ -3,10 +3,17 @@ package vn.phatbee.cosmesticshopapp.retrofit;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import vn.phatbee.cosmesticshopapp.model.Banner;
+import vn.phatbee.cosmesticshopapp.model.LoginRequest;
+import vn.phatbee.cosmesticshopapp.model.LoginResponse;
 
 public interface ApiService {
     @GET("api/banners")
     Call<List<Banner>> getBanners();
+
+    @POST("api/auth/login")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
 }
