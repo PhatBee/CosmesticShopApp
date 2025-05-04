51a3c8569c6a1b2676a3be06ed920ad0a68f5c85
Tama <mytamhuynhhmt@gmail.com>
2025-05-05 02:01:22 +0700

    Checkout UI

---
+++ app/src/main/java/vn/phatbee/cosmesticshopapp/activity/CheckoutActivity.java
@@ -0,0 +1,21 @@
+package vn.phatbee.cosmesticshopapp.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.phatbee.cosmesticshopapp.R;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+
+    }
+}
